948813bb68dff69fb19fe882216d0ff419297abd
Brandon Fernandez <fernandez.b@dhn.com.do>
2023-06-28 17:16:48 -0400

    icons added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { HomeScreen } from "./screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
 
 const Stack = createNativeStackNavigator();
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,15 +3,16 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Bars3CenterLeftIcon } from "react-native-heroicons/outline";
 
-const ios = Platform.OS == "ios";
+const ios = Platform.OS === "ios";
 export const HomeScreen = () => {
   return (
-    <View className="fle-1 bg-neutral-800">
+    <View className="fle-1 bg-red-800">
       {/* Search bar and logo */}
       <SafeAreaView className={ios ? "-mb-2" : "-mb-3"}>
-        <StatusBar style="light" />
-        <View className="flex-row justify-between items-center mx-4">
+        {/* <StatusBar style="dark" /> */}
+        <View className="flex justify-between items-center mx-4">
           <Bars3CenterLeftIcon size={30} strokeWidth={2} color={"white"} />
+          <Text className="text-white text-3xl font-bold">Movies</Text>
         </View>
       </SafeAreaView>
     </View>
