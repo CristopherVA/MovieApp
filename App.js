26385cf961969a99e303fcf81d94e02f9ef30a47
CristopherVA <ing.valentin99@hotmail.com>
2023-06-30 07:39:11 -0300

    movie screen no finish

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { HomeScreen } from "./screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { StatusBar } from "expo-status-bar";
+import MovieScreen from "./screens/MovieScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +13,12 @@ export default function App() {
           name="Home"
           options={{ headerShown: false }}
           component={HomeScreen}
+          initialParams={HomeScreen}
+        />
+        <Stack.Screen
+          name="Movie"
+          options={{ headerShown: false }}
+          component={MovieScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,54 @@
-import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Platform,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Bars3CenterLeftIcon } from "react-native-heroicons/outline";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles } from "../theme";
+import TrendingMovies from "../components/TrendingMovies";
+import MovieList from "../components/MovieList";
 
 const ios = Platform.OS === "ios";
 export const HomeScreen = () => {
+  const [trending, setTrending] = useState([1, 2, 3]);
+  const [upcoming, setUpcoming] = useState([4, 5, 6]);
+  const [topRated, setTopRated] = useState([7, 8, 9]);
+
   return (
-    <View className="fle-1 bg-red-800">
+    <View className="flex-1 bg-neutral-800">
       {/* Search bar and logo */}
-      <SafeAreaView className={ios ? "-mb-2" : "-mb-3"}>
-        {/* <StatusBar style="dark" /> */}
-        <View className="flex justify-between items-center mx-4">
+      <SafeAreaView className={ios ? "-mb-2" : "mt-10 mb-3"}>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx-4">
           <Bars3CenterLeftIcon size={30} strokeWidth={2} color={"white"} />
-          <Text className="text-white text-3xl font-bold">Movies</Text>
+          <Text className="text-white text-3xl font-bold">
+            <Text style={styles.text}>M</Text>ovies
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size="30" strokeWidth={2} color="white" />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
+
+      <ScrollView
+        scrollIndicatorInsets={false}
+        contentContainerStyle={{ paddingBottom: 10 }}
+      >
+        {/* Trending Movies */}
+        <TrendingMovies data={trending} />
+        {/* Movie List Uncoming */}
+        <MovieList title="Upcoming" data={upcoming} />
+        {/* Movie List Rate */}
+        <MovieList title="Upcoming" data={topRated} />
+      </ScrollView>
     </View>
   );
 };
